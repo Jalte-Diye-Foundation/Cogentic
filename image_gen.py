f9354ab0b38fe40ffa5ccd28bab5da7b7a6a0d6e
Soham Sawant <f20240970@goa.bits-pilani.ac.in>
2026-06-30 16:25:50 +0530

    image_gen.py

--- image_gen.py
+++ image_gen.py
@@ -20,7 +20,7 @@ THEME_REGISTRY = {
         "expl_align": "LEFT",
         "margin_left_ratio": 0.20,
         "margin_right_ratio": 0.20,
-        "center_zone_top_ratio": 0.20,
+        "center_zone_top_ratio": 0.25,
         "center_zone_bottom_ratio": 0.46,
     },
     "Women Empowerment": {
@@ -70,7 +70,7 @@ THEME_REGISTRY = {
         "expl_align": "LEFT",
         "margin_left_ratio": 0.20,
         "margin_right_ratio": 0.20,
-        "center_zone_top_ratio": 0.20,
+        "center_zone_top_ratio": 0.25,
         "center_zone_bottom_ratio": 0.46,
     },
     "jdf_general": {
